3b1c03b1f4dc48285508eccc2b3f6d908c996c02
mbMosman <mbmosman@users.noreply.github.com>
2016-08-25 18:46:21 -0500

    initial add

---
+++ README.md
@@ -0,0 +1,1 @@
+# ics440-drawing-lab
---
+++ src/java/com/abc/draw/Drawing.java
@@ -0,0 +1,21 @@
+package com.abc.draw;
+
+import java.awt.*;
+
+public class Drawing extends Object {
+    // FIXME - probably need a member variable or two...
+    
+	public Drawing() {
+        // FIXME - initialize
+	}
+
+	public void drawAll(Graphics2D g2) {
+        // FIXME - Invoke draw(Graphics2D) on each of the Drawable's that
+        //         have been added via append(Drawable).
+	}
+
+	public void append(Drawable drawable) {
+	    // FIXME - Add the passed Drawable to the list of items that make up
+        //         this Drawing.
+	}
+}
