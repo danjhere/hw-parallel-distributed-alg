836527fd48eae5c2ed56454f285d4c8a56530520
mbMosman <mbmosman@users.noreply.github.com>
2016-09-01 23:00:26 -0500

    initial add

---
+++ src/java/com/abc/fruit/Fruit.java
@@ -0,0 +1,56 @@
+package com.abc.fruit;
+
+/**
+ *  Fruit is a self-running object.  (It starts up an internal thread during construction).
+ *
+ * The internal thread for a Fruit instance will print the name supplied at construction 20 times.
+ * Optionally, the internal thread may sleep for a short period of time between printing lines.
+ */
+public class Fruit {
+
+    private Thread internalThread;
+
+    private volatile boolean noStopRequested;
+
+    private String name;
+
+    private int linesToPrint = 20;
+
+    public Fruit(String name) {
+
+        this.name = name;
+        this.noStopRequested = true;
+
+        internalThread = new Thread( new Runnable() {
+            @Override
+            public void run() {
+                doWork();
+            }
+        }, name);
+
+        internalThread.run();
+    }
+
+    private void doWork() {
+
+        int i=0;
+        while (noStopRequested && i<linesToPrint){
+            System.out.println(name);
+            try {
+                internalThread.sleep(5);
+            } catch ( InterruptedException e ) {
+                System.err.println("InterruptedException " + e.getMessage());
+            }
+            i++;
+        }
+    }
+
+    public void stopRequest() {
+        noStopRequested = false;
+        internalThread.interrupt();
+    }
+
+    public boolean isAlive() {
+        return internalThread.isAlive();
+    }
+}
---
+++ src/java/com/abc/fruit/FruitDemo.java
@@ -0,0 +1,16 @@
+package com.abc.fruit;
+
+/*
+ * This class will create at least two instances of Fruit, each with a different name.
+ * Each fruit instance shall print 20 lines of output.
+ */
+public class FruitDemo {
+
+    public static void main(String[] args) {
+	    // Create 2 instances of fruit, each with a different name.
+        Fruit fruit1 = new Fruit("Apple");
+        Fruit fruit2 = new Fruit("Banana");
+        Fruit fruit3 = new Fruit("Rice Ball!");
+
+    }
+}
